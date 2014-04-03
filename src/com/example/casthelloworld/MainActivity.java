b11b39089bf22ec89ddbc07ec3426aa507a7977f
Eluvatar <hahn.kev@gmail.com>
2014-04-02 23:13:40 -0700

    custom receiver changes

--- src/com/example/casthelloworld/MainActivity.java
+++ src/com/example/casthelloworld/MainActivity.java
@@ -1,53 +1,43 @@
 /*
- * Copyright (C) 2014 Google Inc. All Rights Reserved. 
+ * Copyright (C) 2014 Google Inc. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at 
+ * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software 
+ * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and 
+ * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
 package com.example.casthelloworld;
-
-import java.io.IOException;
-import java.util.ArrayList;
 
 import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.speech.RecognizerIntent;
 import android.support.v4.view.MenuItemCompat;
-import android.support.v7.app.ActionBar;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.MediaRouteActionProvider;
+import android.support.v7.app.*;
 import android.support.v7.media.MediaRouteSelector;
 import android.support.v7.media.MediaRouter;
 import android.support.v7.media.MediaRouter.RouteInfo;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
+import android.view.*;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.Toast;
-
-import com.google.android.gms.cast.ApplicationMetadata;
-import com.google.android.gms.cast.Cast;
+import com.google.android.gms.cast.*;
 import com.google.android.gms.cast.Cast.ApplicationConnectionResult;
 import com.google.android.gms.cast.Cast.MessageReceivedCallback;
-import com.google.android.gms.cast.CastDevice;
-import com.google.android.gms.cast.CastMediaControlIntent;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
+import com.google.android.gms.common.api.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Main activity to send messages to the receiver.
@@ -84,9 +74,11 @@ public class MainActivity extends ActionBarActivity {
 		// get text
 		Button voiceButton = (Button) findViewById(R.id.voiceButton);
 		voiceButton.setOnClickListener(new OnClickListener() {
+			int i;
 			@Override
 			public void onClick(View v) {
-				startVoiceRecognitionActivity();
+				sendMessage("this is a test:" + i++);
+//				startVoiceRecognitionActivity();
 			}
 		});
 
@@ -113,7 +105,7 @@ public class MainActivity extends ActionBarActivity {
 
 	/*
 	 * Handle the voice recognition response
-	 * 
+	 *
 	 * @see android.support.v4.app.FragmentActivity#onActivityResult(int, int,
 	 * android.content.Intent)
 	 */
@@ -373,7 +365,7 @@ public class MainActivity extends ActionBarActivity {
 
 	/**
 	 * Send a text message to the receiver
-	 * 
+	 *
 	 * @param message
 	 */
 	private void sendMessage(String message) {
